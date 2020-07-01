56c629247ecc2bb0c14d54dbef9800e35dcb5cf4
Mark Liu <ldtheended@163.com>
2020-07-01 22:46:42 +0800

    函数式组件example03

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Custom from "./components/custom";
+import Custom02 from "./components/custom02";
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
         <h1>CSDN</h1>
         <hr />
         {/*{Custom()}*/}
-        <Custom />
+        {/*<Custom />*/}
+        <Custom02 />
     </div>
   );
 }
---
+++ src/components/custom02.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// 继承React.Component类
+export default class Custom02 extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>React.Component - 类!</h2>
+            </div>
+        );
+    }
+
+}
