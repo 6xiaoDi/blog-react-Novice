bfaed32d1adcea0b8cffb01cbbf2bd86d876b459
Mark Liu <ldtheended@163.com>
2020-07-02 12:37:55 +0800

    v0.15-example05

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import Custom from "./components/custom";
 import Custom02 from "./components/custom02";
 import FriendList from "./components/friendList";
 
+let datas = {
+    family: {
+        title: '家人',
+        list: [
+            {name: '爸爸'},
+            {name: '妈妈'}
+        ]
+    },
+    friend: {
+        title: '朋友',
+        list: [
+            {name: '张三'},
+            {name: '李四'},
+            {name: '王五'}
+        ]
+    },
+    customer: {
+        title: '客户',
+        list: [
+            {name: '阿里'},
+            {name: '腾讯'},
+            {name: '头条'}
+        ]
+    }
+};
+
 function App() {
   return (
     <div className="App">
         <h1>CSDN</h1>
         <hr />
-        {/*{Custom()}*/}
+        {Custom(datas)}
         {/*<Custom />*/}
         {/*<Custom02 />*/}
         <FriendList />
