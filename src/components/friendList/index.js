db1459a25ce846db355e0097caff30726163a63b
Mark Liu <ldtheended@163.com>
2020-07-02 13:47:02 +0800

    v0.211-example11-1

--- src/components/friendList/index.js
+++ src/components/friendList/index.js
@@ -3,28 +3,24 @@ import './index.css';
 
 export default class FriendList extends React.Component {
 
-    renderList() {
-        let {datas} = this.props;
-
-        // 取出json对象中所有key生成数组再遍历
-        return Object.keys(datas).map( (key) => {
-                let data = datas[key];
-                return (
-                    <dl key={key} className='friend-group'>
-                        <dt>{data.title}</dt>
-                    </dl>
-                )
-            }
-        )
-    }
-
     render() {
+        //解构数据
+        let {datas} = this.props;
+        console.log(datas);
 
         return(
             <div>
                 <h2>好友列表</h2>
                 <div className="friend-list">
-                    {this.renderList()}
+                    {Object.keys(datas).map( (key) => {
+                            let data = datas[key];
+                            return (
+                                <dl key={key} className='friend-group'>
+                                    <dt>{data.title}</dt>
+                                </dl>
+                            )
+                        }
+                    )}
                 </div>
             </div>
         )
