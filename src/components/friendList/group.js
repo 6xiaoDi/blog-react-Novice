0cde445eb1312a95ffd1fc868344c5e593779f73
Mark Liu <ldtheended@163.com>
2020-07-02 16:42:50 +0800

    v0.24-3-example14-3 （展开和收缩面板：切换面板无响应！）

--- src/components/friendList/group.js
+++ src/components/friendList/group.js
@@ -2,15 +2,27 @@ import React from "react";
 
 export default class Group extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            expanded: false  // 展开收缩状态，默认收缩
+        }
+        // 一般都在构造函数中绑定好this
+        this.toggle = this.toggle.bind(this);
+    }
+
     toggle(e) {
-        console.log(this);
-        console.log(e.target);
+        this.state.expanded = !this.state.expanded;
+        console.log(this.state.expanded);
     }
 
     render() {
         let {title, list} = this.props.data;
+        let {expanded} = this.state;
+
         return (
-            <dl className={['friend-group','expanded'].join(' ')}>
+            <dl className={['friend-group', expanded ? 'expanded' : ''].join(' ')}>
                 <dt onClick={this.toggle.bind(this)}>{title}</dt>
                 {
                     list.map( item =>
