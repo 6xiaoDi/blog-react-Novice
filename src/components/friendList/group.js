c57bf4f452017dc1b2353cef3eb4180cb0763e1c
Mark Liu <ldtheended@163.com>
2020-07-02 16:06:22 +0800

    v0.23-4-example13-4（展开和收缩面板：指定React的JSX的标签中事件函数this指向，查看事件对象）

--- src/components/friendList/group.js
+++ src/components/friendList/group.js
@@ -2,15 +2,16 @@ import React from "react";
 
 export default class Group extends React.Component {
 
-    toggle() {
+    toggle(e) {
         console.log(this);
+        console.log(e.target);
     }
 
     render() {
         let {title, list} = this.props.data;
         return (
             <dl className='friend-group'>
-                <dt onClick={this.toggle}>{title}</dt>
+                <dt onClick={this.toggle.bind(this)}>{title}</dt>
                 {
                     list.map( item =>
                         <dd key={item.name}>
