a79d0ac86e248565f52f7e4c761d94d02acf1216
Mark Liu <ldtheended@163.com>
2020-07-03 16:14:50 +0800

    v1.06-3-example06-3（属性默认值-defaultProps）

--- app/src/components/PropsDefaultValueDemo.js
+++ app/src/components/PropsDefaultValueDemo.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 export default class PropsDefaultValueDemo extends React.Component {
 
+    /**
+     * 给当前组件的props设置默认值
+     */
+    static defaultProps = {
+        max: 10
+    };
+
     constructor(props) {
         super(props);
     }
 
     render() {
-        let max = this.props.max || 1;
         return(
             <div>
-                <h2>值 - {max}</h2>
+                <h2>值 - {this.props.max}</h2>
             </div>
         );
     }
