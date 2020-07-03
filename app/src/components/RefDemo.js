8eca427256a0891cf9917131210ae62e009d7028
Mark Liu <ldtheended@163.com>
2020-07-03 15:39:33 +0800

    v1.05-example05（需求：点击按钮，动态获取p标签内容高度。React.createRef()实现。）

--- app/src/components/RefDemo.js
+++ app/src/components/RefDemo.js
@@ -11,23 +11,26 @@ export default class UnControl extends React.Component {
 
         this.getHeight = this.getHeight.bind(this);
         this.refE1 = null;
+        // 自动生成帮助赋值节点的函数，但是并不是直接就是这个元素标签，得到是一个对象，对象内的current才是真正的标签对象
+        this.refEl2 = React.createRef();
     }
 
     getHeight() {
-        console.log(this.refEl.offsetHeight)
+        console.log(this.refEl2);
+        console.log(this.refEl2.current.offsetHeight);
     }
 
     render() {
         return(
             <div>
                 <button onClick={this.getHeight}>按钮</button>
-                <p ref={el => {
-                    console.log('...', el)
-                    {
-                        this.refEl = el;
-                    }
-                }} style={{background: 'red', color: 'white'}}>{this.state.content}</p>
-
+                {/*<p ref={el => {*/}
+                {/*    console.log('...', el)*/}
+                {/*    {*/}
+                {/*        this.refEl = el;*/}
+                {/*    }*/}
+                {/*}} style={{background: 'red', color: 'white'}}>{this.state.content}</p>*/}
+                <p ref={this.refEl2} style={{background: 'red', color: 'white'}}>{this.state.content}</p>
             </div>
         );
     }
