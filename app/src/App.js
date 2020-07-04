c573f21d19959910112a6f0934a946d765e9cb63
Mark Liu <ldtheended@163.com>
2020-07-04 14:07:23 +0800

    v2.02-example02（生命周期-getDerivedStateFromProps）

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import LifeCycleDemo from "./components/LifeCycleDemo";
 
-function App() {
-  return (
-    <div className="App">
-      <LifeCycleDemo/>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pVal: 1
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <LifeCycleDemo val={this.state.pVal}/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- app/src/components/LifeCycleDemo.js
+++ app/src/components/LifeCycleDemo.js
@@ -5,7 +5,17 @@ export default class LifeCycleDemo extends React.Component {
     // 组件当中生命周期的第一个函数
     constructor(props) {
         super(props);
+
+        this.state = {
+            a: 1
+        };
+
         console.log('组件初始化');
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        // 从props中获取派生的state
+        console.log('getDerivedStateFromProps', props, state);
     }
 
     render() {
