90be6b35041ef113ef1f85a74c73d5cf72005204
Mark Liu <ldtheended@163.com>
2020-07-04 16:14:38 +0800

    深入探究getDerivedStateFromProps和组件渲染，父组件自己的状态变化就会导致当前的子组件重新渲染。

--- app/src/App.js
+++ app/src/App.js
@@ -14,6 +14,12 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
+                <button onClick={() => {
+                    this.setState({
+                        pVal: this.state.pVal + 1
+                    })
+                }}>父组件的按钮</button>
+                <hr/>
                 <LifeCycleDemo val={this.state.pVal}/>
             </div>
         )
