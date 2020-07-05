1d63410e311fccfd64e9546116be854c282086c4
Mark Liu <ldtheended@163.com>
2020-07-05 20:31:35 +0800

    v3.03-example03（应用场景（二）- 如何给路由组件的组件参数传值？）

--- app/src/App.js
+++ app/src/App.js
@@ -9,6 +9,41 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            items: [
+                {
+                    id: 1,
+                    name: 'iPhone XR',
+                    price: 542500
+                },
+                {
+                    id: 2,
+                    name: 'Apple iPad Air 3',
+                    price: 377700
+                },
+                {
+                    id: 3,
+                    name: 'Macbook Pro 15.4',
+                    price: 1949900
+                },
+                {
+                    id: 4,
+                    name: 'Apple iMac',
+                    price: 1629900
+                },
+                {
+                    id: 5,
+                    name: 'Apple Magic Mouse',
+                    price: 72900
+                },
+                {
+                    id: 6,
+                    name: 'Apple Watch Series 4',
+                    price: 599900
+                }
+            ]
+        }
     }
 
     render() {
@@ -22,8 +57,7 @@ class App extends React.Component {
                     <Link to="/about">关于我们</Link>
                 </nav>
                 <hr/>
-                <input type="text"/>
-                <Route path="/" component={Home} exact />
+                <Route path="/" component={Home} exact items={this.state.items} />
                 <Route path="/about" component={About}/>
             </div>
         )
--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default class Home extends React.Component {
 
     render() {
+        console.log(this.props);
         return (
             <div>
                 <h2>商品列表</h2>
