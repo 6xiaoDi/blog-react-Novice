9dab0347266e900d7f0edc2da7e2fed15e99e3c4
Mark Liu <ldtheended@163.com>
2020-07-05 17:20:07 +0800

    v3.02-3-example02-3（应用场景（一）- React路由精确匹配，UI的方式控制）

--- app/src/App.js
+++ app/src/App.js
@@ -16,9 +16,13 @@ class App extends React.Component {
             <div className="App">
                 <h1>React路由</h1>
                 <hr/>
-                {/*<Route path="/" component={Home}/>*/}
                 <Route path="/" component={Home} exact />
                 <Route path="/about" component={About}/>
+                <nav>
+                    <a href="#/">首页</a>
+                    <span> | </span>
+                    <a href="#/about">关于我们</a>
+                </nav>
             </div>
         )
     }
