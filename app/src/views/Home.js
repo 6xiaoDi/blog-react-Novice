2f474de5fc9d7aaf08faa25bf83a15989eab7928
Mark Liu <ldtheended@163.com>
2020-07-06 12:03:30 +0800

    v3.08-2-example08-2（应用场景（三）- 用户可以按照商品价格的高低进行选择展示-js实现-实现切换排序）

--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -4,17 +4,38 @@ import Item from '../components/Item.js';
 
 export default class Home extends React.Component {
 
-    render() {
-        
+    constructor(props) {
+        super(props);
+
+        this.sort = this.sort.bind(this);
+    }
+
+    sort(e) {
+        console.log(e.target.value);
+
         let {items} = this.props;
 
         items = items.sort( (a, b) => {
-            return b.price - a.price;
+            return 'desc' === e.target.value ? b.price - a.price : a.price - b.price;
         } );
+
+        this.setState({
+            items
+        });
+    }
+
+    render() {
+
+        let {items} = this.props;
 
         return (
             <div>
                 <h2>商品列表</h2>
+
+                <select onChange={this.sort}>
+                    <option value="desc">降序</option>
+                    <option value="asc">升序</option>
+                </select>
 
                 <ul className="item-list">
                     <li className="head">
