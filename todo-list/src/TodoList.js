eed249747810e3b667fe276355a8926c7858d4f3
6xiaodi-mac <ldtheended@163.com>
2021-04-18 20:59:32 +0800

    a0.02-实例：TodoList——框子

---
+++ todo-list/src/TodoList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>todoList</div>
+        )
+    }
+}
+
+export default TodoList
--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TodoList from "./TodoList";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
