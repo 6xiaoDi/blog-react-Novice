b0a3f76fc83b776f0946de808e4788b444d799ff
6xiaodi-mac <ldtheended@163.com>
2021-04-19 22:40:26 +0800

    a0.05-实例：TodoList——提交按钮新增task

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -4,8 +4,9 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputVal: 'helloWorld',
-            list: []
+            // inputVal: 'helloWorld',
+            inputVal: '',
+            list: ["数学","英语","语文"]
         }
     }
 
@@ -14,11 +15,14 @@ class TodoList extends Component {
             <Fragment>
                 <div>
                     <input type="text" value={this.state.inputVal} onChange={this.inputHandle.bind(this)}/>
-                    <button>提交</button>
+                    <button onClick={this.submitTask.bind(this)}>提交</button>
                 </div>
                 <ul>
-                    <li>语文</li>
-                    <li>数学</li>
+                    {
+                        this.state.list.map((val, index) => {
+                            return <li key={index}>{val}</li>
+                        })
+                    }
                 </ul>
             </Fragment>
         )
@@ -29,6 +33,13 @@ class TodoList extends Component {
         // this.state.inputVal = e.target.value
         this.setState({
             inputVal: e.target.value,
+        })
+    }
+
+    submitTask() {
+        this.setState({
+            list: [...this.state.list, this.state.inputVal],
+            inputVal: "",
         })
     }
 }
